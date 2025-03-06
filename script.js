72d95b3294179c5c0184b62fb63f068b515debdc
Natnael Dejene <thecodegeek1@gmail.com>
2025-03-06 14:18:30 +0300

    add form validation and interactivity

---
+++ script.js
@@ -0,0 +1,29 @@
+const errorIcon = document.querySelector('.error-icon');
+const errorMsg = document.querySelector('.error-msg');
+const btn = document.querySelector('button');
+const input = document.querySelector('input');
+
+console.log(errorIcon);
+console.log(errorMsg);
+console.log(btn);
+
+btn.addEventListener('click', (e) => {
+
+    e.preventDefault();
+    const email = document.querySelector('input').value;
+
+    let pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!pattern.test(email)) {
+        errorIcon.classList.remove('hidden');
+        errorMsg.classList.remove('hidden');
+        input.style.borderColor = 'red';
+    } else {
+        errorIcon.classList.add('hidden');
+        errorMsg.classList.add('hidden');
+        document.getElementById('email').value = '';
+        input.style.borderColor = 'hsl(0, 36%, 70%)';
+    }
+});
+
+
+
